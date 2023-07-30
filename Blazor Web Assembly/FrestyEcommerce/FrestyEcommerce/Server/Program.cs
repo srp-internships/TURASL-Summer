9c9f34f0429c97fa0e8256465a83c343ee03b346
endysonendy <endysonendy@gmail.com>
2023-07-30 18:02:28 +0500

    CRUD for Product Type and Categories models

--- Blazor Web Assembly/FrestyEcommerce/FrestyEcommerce/Server/Program.cs
+++ Blazor Web Assembly/FrestyEcommerce/FrestyEcommerce/Server/Program.cs
@@ -8,6 +8,7 @@ global using FrestyEcommerce.Server.Services.AuthService;
 global using FrestyEcommerce.Server.Services.CartService;
 global using FrestyEcommerce.Server.Services.PaymentService;
 global using FrestyEcommerce.Server.Services.AddressService;
+global using FrestyEcommerce.Server.Services.ProductTypeService;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -33,6 +34,7 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
